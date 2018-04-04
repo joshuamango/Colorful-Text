47e83844bb017dac981b587d0e19f3f49271d8e9
Joshua <37916234+joshuamango@users.noreply.github.com>
2018-04-04 15:24:55 -0400

    Main Source Files + Unsigned APK
    Includes: 
    - Main and Display Message java/xml 
    - Manifest, Colors, Strings, and Styles 
    - Unsigned app release apk

---
+++ src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,45 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextViews
+        TextView[] textViewArray = {
+                findViewById(R.id.textView),
+                findViewById(R.id.textView2),
+                findViewById(R.id.textView3),
+                findViewById(R.id.textView4),
+                findViewById(R.id.textView5),
+                findViewById(R.id.textView6),
+                findViewById(R.id.textView7),
+                findViewById(R.id.textView8)
+        };
+
+        // Add the same message to each textView object
+        for (TextView textView : textViewArray) {
+            textView.setText(message);
+        }
+
+        // Set a random text color for each TextView
+        for (TextView textView : textViewArray) {
+            textView.setTextColor(Color.rgb((int)(Math.random() * 255),
+                    (int)(Math.random() * 255), (int)(Math.random() * 255 )));
+        }
+    }
+}
